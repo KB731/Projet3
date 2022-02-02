73c8b97babaa021f2438ccccdc906ce7f2640894
Bebert_73 <75555568+Bebert73@users.noreply.github.com>
2022-02-02 09:35:37 +0100

    ajout (calendrier) Form

--- index.php
+++ index.php
@@ -14,6 +14,22 @@ $result = $query->fetchAll(PDO::FETCH_ASSOC);
     <title>Le Bon Gite</title>
 </head>
 <body>
+<form action="" method="get" class="form-example">
+<label for="start">Date du sejours : </label>
+
+<input type="date" id="dates" name="date-start"
+       value="2018-07-22"
+       min="?" max="?"> 
+       <!-- Trouver comment faire en sorte que la personne ne peut pas choisir avant la date du jours -->
+<input type="date" id="datee" name="date-end"
+       value="2018-07-22"
+       min="" max="">
+  <div class="form-example">  
+
+
+    <input type="submit" value="Subscribe!">
+  </div>
+</form>
 
 </body>
 </html>
