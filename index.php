7ac4762edcc49273a774d4cae1064a16eb6c15dd
Bebert_73 <75555568+Bebert73@users.noreply.github.com>
2022-02-02 09:07:11 +0100

    test

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once('database_connexion.php');
+$sql = 'SELECT * FROM `liste`';
+$query = $db->prepare($sql);
+$query->execute();
+$result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Le Bon Gite</title>
+</head>
+<body>
+    
+</body>
+</html>
