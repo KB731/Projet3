a1d60c8517c79d0b3f2f4d45640f2d2a7d5e2385
Bebert_73 <75555568+Bebert73@users.noreply.github.com>
2022-02-03 10:36:59 +0100

    Ajout form Gite creation

--- admin.php
+++ admin.php
@@ -26,6 +26,7 @@ if (isset($_POST['creer_tache'])) { // On vérifie que la variable POST existe
     <title>Admin Panel</title>
 </head>
 <body>
+<H1>Crée une catégorie </H1>
        <form class="taches_input" method="post" action="admin.php">
            <label for="text">Nom Catégorie</label>
         <input id="inserer" type="text" name="creer_tache"/>
@@ -40,3 +41,47 @@ if (isset($erreurs)) {
 ?>
 </body>
 </html>
+
+
+<?php
+if(isset($_POST['create_gite'])){
+        if (empty($_POST['create_gite'])) {  // On vérifie qu'elle a une valeur
+        $erreurs = '<h1> Vous devez indiquer la valeure de la tâche </h1>';
+    } else {
+        $tache = $_POST['creer_tache'];
+        $db->exec("INSERT INTO category(name) VALUES('$tache')"); // On insère la tâche dans la base de donnée
+        echo ' Catégorie Crée ! ';
+    }
+}
+?>
+<H1>Crée un nouveau gite </H1>
+<form class="taches_input" method="post" action="admin.php">
+           <label for="text">Nom du gite</label>
+        <input id="inserer" type="text" name="create_gite"/>
+        <form></form>
+        <label for="text">Description</label>
+        <input id="inserer" type="text" name="create_desc"/>
+        <form></form>
+        <input type="file" name="create_image" />
+        <form></form>
+        <label>Nombre de lits</label>
+        <input type="number" name="create_bed" min="1" max="15" value="1">
+        <form></form>
+        <label>Nombre de salles de bains</label>
+        <input type="number" name="create_bath" min="1" max="15" value="1">
+        <form></form>
+        <label for="text">Localisation</label>
+        <input id="inserer" type="text" name="create_loc"/>
+        <form></form>
+        <label for="text">Prix</label>
+        <input id="inserer" type="number" name="create_price"/>
+        <form></form>
+        <label for="text">Disponible</label>
+  <input type="checkbox" name="dispo_yes">
+  <label for="scales">oui</label>
+  <input type="checkbox" name="dispo_no">
+  <label for="horns">non</label>
+
+        <form></form>
+        <button id="envoyer">Créer</button>
+    </form>
