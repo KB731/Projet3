8b31f995e212de376c356e8fdefcd40f4a5da2d8
Albert Monteiro <75555568+Bebert73@users.noreply.github.com>
2022-02-09 09:27:42 +0100

    Delete Projet3-master directory

--- Projet3-master/database_connexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-try{
-    $db = new PDO('mysql:host=localhost;dbname=bd_lebongite', 'root','');
-    $db->exec("UTF8");
-} catch (PDOException $e){
-    echo 'Erreur : '. $e->getMessage();
-    die();
-}
-
